a34a9324055f5d2c71b80b2f62e187429baf73d6
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-09-24 22:17:29 -0300

    Explorando el dataset.

---
+++ emoji_data_explorer/exploracion_dataset.R
@@ -0,0 +1,38 @@
+library("dplyr")   # Para transformar la data, y piping `%>%`
+library("forcats") # Para reordenar las barras del plot.
+library("ggplot2") # Para generar los plots.
+library("readr")   # Para cargar el dataset.
+library("tidyr")   # Para pivotear la tabla.
+
+emo_datos <- read_rds("Datos/emo_datos.rds")
+emoji_img_map <- read_rds("Datos/emoji_img_map.rds")
+
+## Veamos un grafico de baras con la cantidad de veces que se eligio cada emoji.
+# Transformamos los datos para el plot.
+datos_plot <- emo_datos %>%
+  select(-pais) %>% # Seleccionamos todo menos la columna de pais (vamos a ver solo los emoji).
+  pivot_longer(cols = starts_with("top_")) %>% # No tenemos en cuenta el orden de eleccion.
+  count(value, sort = TRUE) %>% # Contamos cuantas veces se repite cada emoji.
+  mutate(x = value) # Renombramos la variable `value` como `x` (va a ser nuestro eje x del plot).
+
+# Generamos el grafico de barras.
+ggplot(
+  datos_plot,
+  aes(
+    x = fct_reorder(x, n, .desc = TRUE), # Reordenamos las barras de mayor a menor (repeticiones).
+    y = n
+  )
+) +
+  geom_col() + # Grafico de barras.
+  labs(x = NULL) # Borramos el label del eje x.
+
+# Funcion para generar el plot.
+# `data_conteos` debe ser un data.frame con columnas `x` y `n`.
+plot_barras <- function(data_conteos) {
+  # Reordenamos de mayor a menor los datos del eje x.
+  ggplot(data_conteos, aes(x = fct_reorder(x, n, .desc = TRUE), y = n)) +
+    geom_col() +                       # Grafico de barras.
+    labs(x = NULL)                     # Borramos el label del eje x.
+}
+
+plot_barras(datos_plot)
