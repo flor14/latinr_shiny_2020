b47c1e6744308601383c8e5aaebed6fbe95074fe
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-09-24 21:17:05 -0300

    terminando la UI.

--- emoji_data_explorer/ejercicio_1_final.R
+++ emoji_data_explorer/ejercicio_1_final.R
@@ -18,6 +18,16 @@ ui <- fluidPage(
         multiple = TRUE                # Permite seleccionar mas de uno.
       ),
       plotOutput("por_emoji")          # Lugar donde ira el plot para mostrar el grafico por emoji.
+    ),
+    tabPanel(                          # Un tab para analisis por paises.
+      "Por pais",                      # Titulo del tab.
+      selectInput(                     # Input de selector de opciones.
+        "selector_pais",               # ID del selector de paises.
+        label = "Paises",              # Label del selector.
+        choices = NULL,                # Opciones posibles para seleccionar (NULL por ahora).
+        multiple = TRUE                # Permite seleccionar mas de uno.
+      ),
+      plotOutput("por_pais")           # Lugar donde ira el plot para mostrar el grafico por paises.
     )
   )
 )
