bdc849c5e9c1ae08d522526a95ca6fb6fb84856e
Florencia D'Andrea <florencia.dandrea@gmail.com>
2020-09-27 20:01:22 -0300

    elimine spinner! eso va en la parte 3

--- emoji_data_explorer/ejercicio_1_final.R
+++ emoji_data_explorer/ejercicio_1_final.R
@@ -5,34 +5,36 @@ library("shinythemes") # Para cambiarle el tema de colores.
 
 ########## Interfaz de usuarie.
 
-ui <- fluidPage(
-  title = "Emoji Data Explorer",       # Titulo para la barrita del navegador web.
-  theme = shinytheme("cerulean"),      # Le ponemos un lindo tema de colores!
-  titlePanel("Emoji Data Explorer"),   # Titulo del top de la pagina.
-  navbarPage(                          # Vamos a tener un panel de tabs.
-    title = "Filtrando",               # Titulo de la tabla de tabs.
+ui <- navbarPage(                      # Vamos a tener un panel de tabs.
+    title = "Emoji Data Explorer",     # Titulo de la tabla de tabs.
+    theme = shinytheme("cerulean"),    # Le ponemos un lindo tema de colores!
     tabPanel(                          # Un tab para analisis por emoji.
       "Por emoji",                     # Titulo del tab.
       selectInput(                     # Input de selector de opciones.
         "selector_emoji",              # ID del selector de emojis.
         label = "Emoji",               # Label del selector.
-        choices = NULL,                # Opciones posibles para seleccionar (NULL por ahora).
+        choices = unique(unlist(emo_datos[, -1])), # Opciones posibles para seleccionar.
         multiple = TRUE                # Permite seleccionar mas de uno.
       ),
-      plotOutput("por_emoji")          # Lugar donde ira el plot para mostrar el grafico por emoji.
+                                       
+      #  plotOutput("por_emoji")       # Lugar donde ira el plot para mostrar el grafico por emoji.
+      
     ),
     tabPanel(                          # Un tab para analisis por paises.
       "Por pais",                      # Titulo del tab.
       selectInput(                     # Input de selector de opciones.
         "selector_pais",               # ID del selector de paises.
         label = "Paises",              # Label del selector.
-        choices = NULL,                # Opciones posibles para seleccionar (NULL por ahora).
+        choices = unique(unlist(emo_datos$pais)), # Opciones posibles para seleccionar.
         multiple = TRUE                # Permite seleccionar mas de uno.
       ),
-      plotOutput("por_pais")           # Lugar donde ira el plot para mostrar el grafico por paises.
-    )
+                                      
+       # plotOutput("por_pais")        # Lugar donde ira el plot para mostrar el grafico por paises.
+     
+    ),
+  tabPanel("Licencia",  tags$a(href="https://github.com/flor14/latinr_shiny_2020/blob/master/LICENSE.md", "Archivo con licencia MIT"))
   )
-)
+
 
 ########## Codigo de servidor.
 
